f89039b63274f22752815193af7329c492bc7554
Tquandoo <quantrungdo2401@gmail.com>
2023-06-01 23:41:17 +0700

    login

---
+++ login/app.js
@@ -0,0 +1,22 @@
+let showPassword = document.getElementById('login__group-showicon');
+let inputPassword= document.getElementById('password');
+showPassword.onclick = function(){
+    if(inputPassword.type == 'password'){
+        inputPassword.type = 'text';
+        showPassword.classList.add('show');
+    }else{
+        inputPassword.type = 'password';
+        showPassword.classList.remove('show');
+    }
+}
+function login(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if(password == "24012003"){
+        window.localStorage.setItem("user", username);
+        window.location.href = "home.html"
+    }else{
+        alert("Sai thong tin dang nhap!");
+    }
+}
